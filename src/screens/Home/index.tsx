cee87b01cb7812b4809598e4a01b0b672f728338
Leandro Martins <leandro.martins.85@gmail.com>
2023-12-08 17:01:10 -0300

    Organizando o projeto

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+import { styles } from "./styles";
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Alan is my witch!</Text>
+    </View>
+  )  
+}
