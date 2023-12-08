c201cbda880894de6d67e7cccf5e60dede933b1b
Leandro Martins <leandro.martins.85@gmail.com>
2023-12-08 18:20:36 -0300

    Criando Componente Participant

---
+++ src/components/Participant/index.tsx
@@ -0,0 +1,18 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+
+export function Participant() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>
+        Leandro Martins
+      </Text>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>
+          -
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text, TextInput, TouchableOpacity } from "react-native";
 
 import { styles } from "./styles";
+import { Participant } from "../../components/Participant";
 
 export function Home() {
   function handleParticipantAdd() {
@@ -28,6 +29,10 @@ export function Home() {
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+
+      <Participant />
+      <Participant />
+      <Participant />
   </View>
   )  
 }
