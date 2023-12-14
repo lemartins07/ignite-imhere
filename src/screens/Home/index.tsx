1e60a6d5d828aef7589294ea9571103a4485d81b
Leandro Martins <leandro.martins.85@gmail.com>
2023-12-13 22:48:00 -0300

    Removendo Participante

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -31,7 +31,9 @@ export function Home() {
     Alert.alert("Remover", `Remover o participante ${name}?`, [
       {
         text: 'Sim',
-        onPress: () => Alert.alert("Deletado!")
+        onPress: () => setParticipants(
+          prevState => prevState.filter(p => p !== name)
+        )
       },
       {
         text: 'Não',
